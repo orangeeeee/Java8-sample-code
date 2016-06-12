c1dd3c020f61100312357c28df0c842ff51eb0e4
air mac <orangeeeee.ore.ore@gmail.com>
2016-06-12 23:05:33 +0900

    パターンを追加

--- src/sample/logic/DateTest.java
+++ src/sample/logic/DateTest.java
@@ -1,20 +1,29 @@
 package sample.logic;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class DateTest {
 
 	public void excecute() {
 
-		DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 		ZonedDateTime z1 = ZonedDateTime.now();
-	    Instant i3 = z1.toInstant();
-	    ZonedDateTime z3 = i3.atZone(ZoneId.of("Asia/Tokyo"));
-	    System.out.println(z3.format(f));
+		Instant i3 = z1.toInstant();
+		ZonedDateTime z3 = i3.atZone(ZoneId.of("Asia/Tokyo"));
+		System.out.println("pattern1(Asia/Tokyo):" + z3.format(fmt));
+
+		//pattern1と同じ結果になる。
+		Date currentDate = new Date();
+		Instant now = currentDate.toInstant();
+		ZoneId currentZone = ZoneId.systemDefault();
+		ZonedDateTime z3_1 = ZonedDateTime.ofInstant(now, currentZone);
+		System.out.println("pattern2(" + z3_1.getZone() + "):" + z3_1.format(fmt));
 		
+		ZonedDateTime z3_2 = i3.atZone(ZoneId.of("Europe/Paris"));
+		System.out.println("pattern3(Europe/Paris):" + z3_2.format(fmt));
 	}
 }
