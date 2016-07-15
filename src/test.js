727193063ac03b6e361889d3779dbbe3f2bcc723
air mac <orangeeeee.ore.ore@gmail.com>
2016-07-15 22:06:42 +0900

    ja

--- src/test.js
+++ src/test.js
@@ -1,29 +1,56 @@
 $(function () {
-    
-    //functionの引数にfunctionを渡さないといけない。
-    
-    $("#dialogButton").click(function () {
-        /* 必要なときに、必要なdiv要素を生成して、利用する */
+
+    function _defaultDialogObj() {
+                        /* 必要なときに、必要なdiv要素を生成して、利用する */
         var x = $("<div></div>").dialog({
             autoOpen: false
         });
-        /* ダイアログの中身は、どこかから取ってきたり、べた書きしたり */
-        x.html('<div class="message">message</h1>');
+//        /* ダイアログの中身は、どこかから取ってきたり、べた書きしたり */
+//        x.html('<div class="message">messages</h1>');
+        
         /* ダイアログのオプションを設定して */
-        x.dialog("option", {
-            title: "ダイアログのタイトル",
+        var defaultDialogObject = x.dialog("option", {
+            title: "警告",
             width: 300,
             height: 200,
             modal: true,
             resizable : false,
-            draggable : false, // ドラッグできないようにする。
+            draggable : false
+        });
+        
+        return defaultDialogObject;
+    }
+    /**
+    *
+    */
+    function confirmDialog(message, callback) {
+        
+        
+        var dialogObj = _defaultDialogObj();
+        
+        /* ダイアログの中身は、どこかから取ってきたり、べた書きしたり */
+        dialogObj.html('<div class="message">' + message + '</h1>');
+        
+        dialogObj.dialog("option", {
             buttons: {
-                "OK": function () {
+                "OK": function() {
                     $(this).dialog("close");
+                    callback();
+                },
+                "キャンセル" : function() {
+                    $(this).dialog("close");   
                 }
             }
-        }).prev(".ui-dialog-titlebar").css("background","#2E2EFE");
+        });
+        
+        dialogObj.prev(".ui-dialog-titlebar").css("background","#CCDCFF");
         /* ダイアログを開きます */
-        x.dialog("open");
+        dialogObj.dialog("open");
+    }
+    
+    
+    $("#dialogButton").click(function () {
+        var message = "本当に削除してもよろしいですか？";
+        confirmDialog(message, function() {alert('aaaa');});
     });
 });
