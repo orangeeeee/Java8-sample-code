cde3674b7cd52a6db2f92969f4f92847caf0d636
air mac <orangeeeee.ore.ore@gmail.com>
2017-01-21 00:26:40 +0900

    add getName method

--- src/sample/logic/OtherObjectStream.java
+++ src/sample/logic/OtherObjectStream.java
@@ -9,26 +9,40 @@ import emum.kubun;
 import exception.SystemException;
 
 public class OtherObjectStream {
-	
 
 	public void test() {
-		
-		String searchKey = "2";
-		
-		MemberType memberType =  this.get(MemberType.class, searchKey).orElseThrow(SystemException::new);
-		MemberType memberType2 =  this.get(MemberType.class, searchKey).orElse(null);
 
-		//Array.asList
+		String searchKey = "2";
+
+		// Array.asList
 		Arrays.asList(MemberType.values()).stream().filter(mt -> mt.getKey().equals(searchKey)).findFirst();
-		//Array.asList ⇨ Stream.of
+		// Array.asList ⇨ Stream.of
 		Stream.of(MemberType.values()).filter(mt -> mt.getKey().equals(searchKey)).findFirst();
-		
+
+		MemberType memberType = this.get(MemberType.class, searchKey).orElseThrow(SystemException::new);
+		MemberType memberType2 = this.get(MemberType.class, searchKey).orElse(null);
+
+		String name = this.getName(MemberType.class, searchKey);
 	}
-	
+
+	/**
+	 * 指定のキーに対するKubunをimplementsしているenumの型を返す。
+	 * 
+	 * @param clazz
+	 * @param keyValue
+	 * @return Optional<E> 区分値の型
+	 */
 	private <E extends Enum<E>> Optional<E> get(Class<E> clazz, String keyValue) {
-		
-		return Stream.of(clazz.getEnumConstants()).filter(k -> k instanceof kubun && ((kubun) k).getKey().equals(keyValue)).findFirst();
-	
+
+		return Stream.of(clazz.getEnumConstants())
+				.filter(k -> k instanceof kubun && ((kubun) k).getKey().equals(keyValue)).findFirst();
+	}
+	//nameを返す。
+	private <E extends Enum<E>> String getName(Class<E> clazz, String keyValue) {
+
+		Optional<E> result =  Stream.of(clazz.getEnumConstants())
+				.filter(k -> k instanceof kubun && ((kubun) k).getKey().equals(keyValue)).findFirst();
+		return result.isPresent() ? null : result.get().name();
 	}
 
 }
