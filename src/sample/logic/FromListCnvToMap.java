1e384b2f047984eb0e252e9bdabb18403baaa7f7
air mac <orangeeeee.ore.ore@gmail.com>
2017-02-01 19:36:38 +0900

    不要なコードを削除

--- src/sample/logic/FromListCnvToMap.java
+++ src/sample/logic/FromListCnvToMap.java
@@ -55,49 +55,4 @@ public class FromListCnvToMap {
 
 		return res;
 	}
-
-	/**
-	 * 別のクラスに変換して渡す。
-	 * 
-	 * @return
-	 * @throws InvocationTargetException
-	 * @throws IllegalAccessException
-	 */
-	public Map<String, List<DummyMiddleCategory>> convertMap2() {
-
-		CreateCategoryDataList createLogic = new CreateCategoryDataList();
-
-		List<Category> cateList = createLogic.create();
-
-		// Map<String, List<DummyMiddleCategory>> res =
-		// cateList.stream().collect(
-		// Collectors.toMap( Category::getKey, k -> valueMapper1(k))
-		// );
-
-		return null;
-	}
-
-	// commonsのcopypropertyで良いかも。
-	private List<DummyMiddleCategory> valueMapper1(Category category) {
-
-		List<DummyMiddleCategory> dList = new ArrayList<>();
-
-		String categoryKey = category.getKey();
-
-		List<MiddleCategory> mCategoryList = category.getMiddleCategoryList();
-
-		for (MiddleCategory mCategory : mCategoryList) {
-
-			DummyMiddleCategory dummy = new DummyMiddleCategory();
-			// Exceptionの問題発生・・・どうやって呼びもとの例外処理を書こうかな・・・
-			// BeanUtils.copyProperties(dummy, mCategory);
-			dummy.setCategoryKey(categoryKey);
-			dummy.setKey(mCategory.getKey());
-			dummy.setName(mCategory.getName());
-			dList.add(dummy);
-		}
-
-		return dList;
-	}
-
 }
