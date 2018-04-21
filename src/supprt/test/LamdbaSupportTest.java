ecca493ef5b0f33331c487fe897a9698ee2e371a
orangeeeee <orangeeeee.ore.ore@gmail.com>
2018-04-21 09:53:45 +0900

    add method in LamdbaSupport

--- src/supprt/test/LamdbaSupportTest.java
+++ src/supprt/test/LamdbaSupportTest.java
@@ -1,9 +1,15 @@
 package supprt.test;
 
+import static supprt.LambdaSupport.isEqual;
+import static supprt.LambdaSupport.isNotEmpty;
+import static supprt.LambdaSupport.toUpperCase;
+import static supprt.LambdaSupport.opt;
+
 import java.util.Arrays;
 import java.util.List;
 
-import static supprt.LambdaSupport.*;
+import bean.Category;
+import test.data.CreateCategoryDataList;
 
 public class LamdbaSupportTest {
 
@@ -11,16 +17,26 @@ public class LamdbaSupportTest {
 
 		List<String> list = Arrays.asList("a", "b", "", "", "");
 
-		//関数を返す static methodを用意するとシンプルになる。
+		// 関数を返す static methodを用意するとシンプルになる。
 		list.stream().filter(isNotEmpty()).map(toUpperCase()).findFirst();
-		//
-		//
-		// // テストデータ作成
-		// List<Category> cateList = createLogic.create();
-		//
-		// final String searchKey = "BG";
-		// Optional<MiddleCategory> middleCategory =
-		// cateList.stream().filter(predicate)
+
+		String code = "b";
+		// 関数を返す static methodを用意するとシンプルになる。
+		list.stream().filter(isEqual().apply(code)).map(toUpperCase()).findFirst();
+
+		String abcString = null;
+		System.out.println(//
+				opt(abcString).map(toUpperCase()).orElse(null));
+
+		Category category = null;//new Category();
+		System.out.println(//
+				opt(category).orElse(new Category()));
 	}
 
+	private static List<Category> createTestList() {
+
+		// テストデータ作成
+		CreateCategoryDataList createLogic = new CreateCategoryDataList();
+		return createLogic.create();
+	}
 }
